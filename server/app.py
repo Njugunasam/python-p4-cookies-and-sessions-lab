3993637f54bc2a15bf64d1062da93b2f69eb0679
Njugunasam <sammynjuguna5826@gmail.com>
2024-01-31 16:01:13 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -20,15 +20,32 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
-def index_articles():
-
-    pass
-
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Set initial page_views value to 0 if this is the first request
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment page_views for every request
+    session['page_views'] += 1
+    
+    # Retrieve the article by ID
+    article = Article.query.get(id)
+
+    if session['page_views'] <= 3:
+        # Render JSON response with article data
+        return jsonify({
+            'article_id': article.id,
+            'title': article.title,
+            'content': article.content,
+            'author': article.author,
+            'preview': article.preview,
+            'minutes_to_read': article.minutes_to_read,
+            'date': article.date.isoformat(),
+            'user_id': article.user_id
+        })
+    else:
+        # Render JSON response with an error message and status code 401
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
